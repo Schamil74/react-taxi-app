b9d933c3386b54322b126f73931885e0a20827d9
Shamil Minibaev <schamil.minibaevl@gmail.com>
2020-11-13 21:48:16 +0500

    feat(router): remove router

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,12 @@ import 'fontsource-roboto'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { store } from './store/reducers'
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter basename={process.env.REACT_APP_BASENAME}>
-            <App />
-        </BrowserRouter>
+        <App />
     </Provider>,
 
     document.getElementById('root')
